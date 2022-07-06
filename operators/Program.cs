66c1fe3245dca071aaf6900a73a4eeb6434ec8d1
Felipemart123 <felipemart1876@gmail.com>
2022-07-06 13:54:55 -0400

    operators

---
+++ operators/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace OperatorsInClass
+{
+    class program
+    {
+        static void Main(string [] args)
+        {
+            int a = 17;
+
+            int b = 4;
+
+            int remainder = a % b;
+
+            int quotient = a / b;
+
+            var str1 = (a == 17 && b == 4) ? $"17/4 is {quotient} remainder is {remainder}" : "incorrect";
+
+            Console.WriteLine(str1);
+            
+
+
+
+
+
+
+            Console.WriteLine("what is the radius of your circle?");
+            
+
+            var input = Console.ReadLine();
+
+            var radius = double.Parse(input);
+
+            Console.WriteLine(AreaOfCircle(radius));
+
+            AreaOfCircle(radius);
+        }
+
+        public static double AreaOfCircle(double radius)
+
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            
+            return area;
+            
+        }
+    }
+}
+
